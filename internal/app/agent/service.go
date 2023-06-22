8e1c329e0b9cbec4d07a73e69f2e0c8893f29b8d
Vladislav Ramus <vladislaoramos@gmail.com>
2023-06-22 17:36:50 +0300

    return the file

---
+++ internal/app/agent/service.go
@@ -0,0 +1,170 @@
+package agent
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha512"
+	"crypto/x509"
+	"encoding/json"
+	"encoding/pem"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/vladislaoramos/alemetric/internal/entity"
+)
+
+// WebAPIClient implements the client web-application for Agent.
+type WebAPIClient struct {
+	client    *resty.Client
+	Key       string
+	publicKey string
+}
+
+func NewWebAPI(client *resty.Client, key string, cryptoKey string) *WebAPIClient {
+	return &WebAPIClient{
+		client:    client,
+		Key:       key,
+		publicKey: cryptoKey,
+	}
+}
+
+func splitHostPort(baseURL string) (host, port string, err error) {
+	var url, prefix string
+
+	if strings.HasPrefix(baseURL, "http://") {
+		prefix = "http://"
+	}
+
+	if strings.HasPrefix(baseURL, "https://") {
+		prefix = "https://"
+	}
+
+	url = strings.TrimLeft(baseURL, prefix)
+
+	host, port, err = net.SplitHostPort(url)
+	if err != nil {
+		return "", "", err
+	}
+
+	return host, port, nil
+}
+
+// SendMetrics sends a client request for a metrics update to the server.
+func (wc *WebAPIClient) SendMetrics(
+	metricsName,
+	metricsType string,
+	delta *entity.Counter,
+	value *entity.Gauge,
+) error {
+	body := entity.Metrics{
+		ID:    metricsName,
+		MType: metricsType,
+		Delta: delta,
+		Value: value,
+	}
+
+	body.SignData("agent", wc.Key)
+
+	// respBody, _ := json.Marshal(body)
+	// log.Printf("req body: %s", string(respBody))
+
+	b, err := json.Marshal(body)
+	if err != nil {
+		return err
+	}
+
+	publicKey, err := loadPublicKeyFromFile(wc.publicKey)
+	if err != nil {
+		return err
+	}
+
+	encryptedBody := tryEncrypt(b, publicKey)
+
+	realIP, _, err := splitHostPort(wc.client.BaseURL) // нужно передавать что-то другое или написать свой парсер срезая всё до слеша и до двоеточия
+	if err != nil {
+		return err
+	}
+
+	resp, err := wc.client.
+		R().
+		SetHeader("Content-Type", "application/json").
+		SetHeader("X-Real-IP", realIP).
+		SetBody(encryptedBody).
+		Post("/update/")
+	if err != nil {
+		return fmt.Errorf("cannot send metrics from agent: %w", err)
+	}
+
+	status := resp.StatusCode()
+	if status != http.StatusOK {
+		return fmt.Errorf("sending metrics from agent with not successful status code: %d", status)
+	}
+
+	return nil
+}
+
+// SendSeveralMetrics sends a client request for several metrics update to the server.
+func (wc *WebAPIClient) SendSeveralMetrics(items []entity.Metrics) error {
+	resp, err := wc.client.
+		R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(items).
+		Post("/updates/")
+	if err != nil {
+		return fmt.Errorf("cannot send several metrics from agent: %w", err)
+	}
+
+	status := resp.StatusCode()
+	if status != http.StatusOK {
+		return fmt.Errorf("sending several metrics from agent with not successful status code: %d", status)
+	}
+	return nil
+}
+
+func tryEncrypt(msg []byte, key *rsa.PublicKey) []byte {
+	if key == nil {
+		return msg
+	}
+
+	hash := sha512.New()
+
+	result, err := rsa.EncryptOAEP(hash, rand.Reader, key, msg, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return result
+}
+
+func loadPublicKeyFromFile(filePath string) (*rsa.PublicKey, error) {
+	if filePath == "" {
+		return nil, nil
+	}
+
+	keyBytes, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	block, _ := pem.Decode(keyBytes)
+	if block == nil {
+		return nil, fmt.Errorf("failed to decode PEM block")
+	}
+
+	pubKey, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse public key: %w", err)
+	}
+
+	rsaPubKey, ok := pubKey.(*rsa.PublicKey)
+	if !ok {
+		return nil, fmt.Errorf("unexpected public key type")
+	}
+
+	return rsaPubKey, nil
+}
